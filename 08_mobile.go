fb0f598d18644c3b4f95b9707e3d1e57cdbd20c5
Vivek Anandaraman <Vivek.Anandaraman@gmail.com>
2020-08-26 23:48:16 +0530

    geo3

---
+++ 08_mobile.go
@@ -0,0 +1,51 @@
+//program to parse html for mobile number
+
+package main
+
+import (
+				//"os"
+				//"buffer"
+				"fmt"
+        //"strconv"
+				//"io/ioutil"
+        //"strings"
+        //"log"
+        "net/http"
+				"github.com/PuerkitoBio/goquery"
+        //"encoding/json"
+        //"github.com/tidwall/gjson"
+//				"github.com/gocolly/colly"
+)
+
+func main() {
+  client := &http.Client{}
+
+  req, err := http.NewRequest("GET", "https://www.justdial.com/Chennai/Grocery-Stores-in-Selaiyur/nct-10237947/page-1", nil)
+  if err != nil {
+    fmt.Println(err)
+  }
+	fmt.Println("hi")
+  req.Header.Add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36")
+
+  resp, err := client.Do(req)
+  if err != nil {
+    fmt.Println(err)
+  }
+
+  defer resp.Body.Close()
+
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	// parse the contact info from html
+	//doc.Find("p.contact-info span.mobilesv").Each(func(i int, s *goquery.Selection) {
+    //fmt.Println(s.Text())
+	//})
+	jsonData, err := doc.Find("span[class='mobilesv']").Html()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(jsonData)
+}
